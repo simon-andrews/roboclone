15783af5363da06a34b2214ba59d20edeedf6eb2
Simon Andrews <simon.andrews212@gmail.com>
2015-10-29 20:57:14 -0400

    Bump version to 0.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ def read(file):
 
 setup(
     name="roboclone",
-    version="0.2",
+    version="0.3",
     packages=['roboclone'],
     author="Simon Andrews",
     url="https://github.com/simon-andrews/roboclone",
